1a1b875021c83b27c501aa6fdda6e6a5a264c3d7
Ilyas Seisov <ilyasseisov@gmail.com>
2022-11-29 06:00:14 -0700

    update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
+// Variables
 const tabs = document.querySelectorAll('.tab');
 
-tabs.forEach((tab) => {
-  tab.addEventListener('click', (event) => {
-    document.querySelector('.active').classList.remove('active');
-    event.currentTarget.classList.add('active');
+// Functions
+const changeTab = (event) => {
+  // remove class ".active" from the current active tab
+  document.querySelector('.active').classList.remove('active');
 
-    document.querySelector('.show').classList.remove('show');
-    document
-      .querySelector(event.currentTarget.dataset.panel)
-      .classList.add('show');
-  });
+  // add class ".active" to the clicked tab
+  event.currentTarget.classList.add('active');
+
+  // remove class ".show" from the current active panel
+  document.querySelector('.show').classList.remove('show');
+
+  // add class ".show" to the corresponding new active's tab panel
+  document
+    .querySelector(event.currentTarget.dataset.panel)
+    .classList.add('show');
+};
+
+// Event Listeners
+tabs.forEach((tab) => {
+  tab.addEventListener('click', changeTab);
 });
